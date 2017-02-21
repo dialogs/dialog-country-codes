76817d1b04087efc3757bffecd2d0642ee83c209
Oleg Shilov <me@olegshilov.com>
2017-02-21 18:04:41 +0300

    feat: add alpha2 and alpha3 codes, add getEmojiFlag method

--- bin/convert.js
+++ bin/convert.js
@@ -8,13 +8,25 @@ const data = require('./countries.json');
 
 const countries = [];
 
+function getEmojiFlag(code) {
+  const offset = 127397;
+  return String.fromCodePoint(code.charCodeAt(0) + offset, code.charCodeAt(1) + offset)
+}
+
 data.forEach((country) => {
-  countries.push({
-    name: country.name,
+  const data = {
+    alpha2: country.alpha2,
+    alpha3: country.alpha3,
     codes: country.countryCallingCodes,
-    emoji: country.emoji
-  });
+    emoji: country.alpha2 ? getEmojiFlag(country.alpha2) : null
+  };
+
+  if (country.countryCallingCodes.length) {
+    countries.push(data);
+  }
 });
+
+console.log(countries.length)
 
 fs.writeFileSync(
   path.resolve(__dirname, '../countries.json'),
