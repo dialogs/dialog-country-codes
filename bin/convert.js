e0ad1f8a61fd28effc1e7a82bdcfe82fbb4a80e8
Nikita Gusakov <gusnkt@gmail.com>
2017-02-22 18:41:59 +0300

    refactor: provide only one alpha variant

--- bin/convert.js
+++ bin/convert.js
@@ -6,27 +6,20 @@ const fs = require('fs');
 const path = require('path');
 const data = require('./countries.json');
 
-const countries = [];
-
 function getEmojiFlag(code) {
   const offset = 127397;
   return String.fromCodePoint(code.charCodeAt(0) + offset, code.charCodeAt(1) + offset)
 }
 
-data.forEach((country) => {
-  const data = {
-    alpha2: country.alpha2,
-    alpha3: country.alpha3,
+const countries = data.filter((country) => {
+  return country.countryCallingCodes.length;
+}).map((country) => {
+  return {
+    alpha: country.alpha2 || country.alpha3,
     codes: country.countryCallingCodes,
     emoji: country.alpha2 ? getEmojiFlag(country.alpha2) : null
   };
-
-  if (country.countryCallingCodes.length) {
-    countries.push(data);
-  }
 });
-
-console.log(countries.length)
 
 fs.writeFileSync(
   path.resolve(__dirname, '../countries.json'),
