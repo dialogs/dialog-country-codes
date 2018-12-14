b2b2456b6558a4aa28c6a7cf038ca5245eace42b
meafmira <o.nechiporenko@dlg.im>
2018-12-14 15:52:35 +0300

    feat: added native names

--- src/getCountryName.js
+++ src/getCountryName.js
@@ -5,6 +5,7 @@
 
 import en from './meta/en.json';
 import ru from './meta/ru.json';
+import natives from './meta/natives.json';
 
 export function getCountryName(alpha: string, locale: string): string {
   switch (locale) {
@@ -20,6 +21,10 @@ export function getCountryNamesArray(alpha: string): string[] {
 
   names.push(en[alpha]);
   names.push(ru[alpha]);
+
+  if (natives[alpha]) {
+    names.push(...natives[alpha]);
+  }
 
   return names;
 }
